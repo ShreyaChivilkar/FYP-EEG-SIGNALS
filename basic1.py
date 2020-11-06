07d0e75dd2eec5058062c877fecaf4560c8ee7ce
ShreyaChivilkar <47733871+ShreyaChivilkar@users.noreply.github.com>
2020-11-06 22:53:20 +0530

    Extract data

--- basic1.py
+++ basic1.py
@@ -12,6 +12,9 @@ for item in fList:
 
     signals, signal_headers, header = prd.highlevel.read_edf('C:/Users/S/test/BTech/eeg-during-mental-arithmetic-tasks-1.0.0/'+item)
     x.append(signals)
+    
+df = pd.DataFrame(x)
+df.to_csv("File1.csv")
 
 
 
