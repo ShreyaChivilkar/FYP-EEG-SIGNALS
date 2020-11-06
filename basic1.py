25e76fba395236971d44a383f7d974d91869b6a4
ShreyaChivilkar <47733871+ShreyaChivilkar@users.noreply.github.com>
2020-11-06 22:23:24 +0530

    Extract Data from all edf files

---
+++ basic1.py
@@ -0,0 +1,17 @@
+import pyedflib as prd
+import numpy as np
+import pandas as pd
+import os
+
+fList = os.listdir('C:/Users/S/test/BTech/eeg-during-mental-arithmetic-tasks-1.0.0')
+x = []
+
+# read an edf file 
+                  
+for item in fList:
+
+    signals, signal_headers, header = prd.highlevel.read_edf('C:/Users/S/test/BTech/eeg-during-mental-arithmetic-tasks-1.0.0/'+item)
+    x.append(signals)
+
+
+
